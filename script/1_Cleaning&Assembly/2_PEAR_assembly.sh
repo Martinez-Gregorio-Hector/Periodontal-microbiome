340392d2d0f9691d341dcf3a40e97a4062a58a29
Héctor Martínez <53798505+Martinez-Gregorio-Hector@users.noreply.github.com>
2025-01-09 13:31:44 -0600

    Update 2_PEAR_assembly.sh
    add info

--- script/1_Cleaning&Assembly/2_PEAR_assembly.sh
+++ script/1_Cleaning&Assembly/2_PEAR_assembly.sh
@@ -1,1 +1,22 @@
+#!/bin/bash
+
+mkdir PEAR_fastq
+
+# Assembled reads with a Phred quality score greater than 30.
+
+fastq=($(ls raw_data_fastq/*.gz))
+tLen=${#fastq[@]}
+for (( i=0; i<${tLen}; i=i+2));
+do
+x=${fastq[$i]##*/}
+pear -f ${fastq[$i]} -r ${fastq[$i+1]} -o PEAR_fastq/${x%_L*} -j 20 -q 30
+done
+
+# Move only assembled pairs
+
+mkdir PEAR_assambled
+mv PEAR_fastq/*assembled.fastq PEAR_assambled
+
+
+figlet done
 
