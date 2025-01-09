3fc632e5b7a1fca3215e0558daa68494aba4a46f
Héctor Martínez <53798505+Martinez-Gregorio-Hector@users.noreply.github.com>
2025-01-09 13:12:51 -0600

    Update 1_MultiQC_before assembly.sh
    Script for quality data

--- script/1_Cleaning&Assembly/1_MultiQC_before assembly.sh
+++ script/1_Cleaning&Assembly/1_MultiQC_before assembly.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+# fastq and multiqc
+
+fastqc fastq_file/*
+multiqc fastq_file/* .
+
+# mv html and zip file in other directory
+
+mkdir fastq_file/multiqc_html_zip
+mv fastq_file/*html multiqc_html_zip
+mv fastq_file/*zip multiqc_html_zip
